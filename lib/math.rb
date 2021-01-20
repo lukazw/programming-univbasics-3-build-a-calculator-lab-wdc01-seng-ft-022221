87836aa29ae45a3e9f34b1a9809663e1127e482b
Lucas Walter <lucasfromflorida@gmail.com>
2021-01-20 02:59:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,14 +1,19 @@
 def addition(num1, num2)
   result = num1 + num2
   puts result
+  result
 end
 
 def subtraction(num1, num2)
-
+  result = num1 - num2
+  puts result
+  result
 end
 
 def division(num1, num2)
-
+  result = num1/num2
+  puts result
+  result
 end
 
 def multiplication(num1, num2)
