e39a7412b83a042c53a8c98d09934ec5fb7ebe3e
Lucas Walter <lucasfromflorida@gmail.com>
2021-01-20 03:00:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -17,7 +17,9 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-
+  result = num1*num2
+  puts result
+  result
 end
 
 def modulo(num1, num2)
@@ -25,5 +27,7 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-
+  result = num ** num
+  puts result
+  result
 end
